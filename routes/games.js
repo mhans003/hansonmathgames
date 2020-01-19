679c47fa41ad8d9e4e563d234d573a4e52e17954
Michael Hanson <michaeledwardhanson@gmail.com>
2020-01-19 02:39:48 +0000

    added multiple games for the first time

--- routes/games.js
+++ routes/games.js
@@ -6,8 +6,49 @@ router.get("/", function(req, res) {
 	res.render("games/index"); 
 });
 
+//games
 router.get("/multiplication", function(req, res) {
 	res.render("games/multiplication");  
+}); 
+
+router.get("/division", function(req, res) {
+	res.render("games/division");  
+}); 
+
+router.get("/rounding", function(req, res) {
+	res.render("games/rounding");  
+}); 
+
+router.get("/convertingunits", function(req, res) {
+	res.render("games/convertingunits");  
+}); 
+
+router.get("/wordform", function(req, res) {
+	res.render("games/wordform");  
+}); 
+
+router.get("/primecomposite", function(req, res) {
+	res.render("games/primecomposite");  
+}); 
+
+router.get("/primenumbergenerator", function(req, res) {
+	res.render("games/primenumbergenerator");  
+}); 
+
+router.get("/equivalentfractionmaker", function(req, res) {
+	res.render("games/equivalentfractionmaker");  
+}); 
+
+router.get("/simplifiedfractiongenerator", function(req, res) {
+	res.render("games/simplifiedfractiongenerator");  
+}); 
+
+router.get("/preparelongdivision", function(req, res) {
+	res.render("games/preparelongdivision");  
+}); 
+
+router.get("/divisionremainders", function(req, res) {
+	res.render("games/divisionremainders");  
 }); 
 
 module.exports = router; 
