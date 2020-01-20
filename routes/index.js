da5a04507c66f06abc3044ba14044142bff95f6b
Michael Hanson <michaeledwardhanson@gmail.com>
2020-01-20 17:37:17 +0000

    added passport for user registration authentication; can add user via form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require("express"); 
 var router = express.Router(); 
+var passport = require("passport"); 
+var User = require("../models/user"); //correct path? 
 
 //root
 router.get("/", function(req, res) {
 	res.render("main"); 
+});
+
+//AUTHENTICATION
+
+//register
+router.get("/register", function(req, res) {
+	res.render("register"); //inc. - see reference
 }); 
+
+router.post("/register", function(req, res) {
+	console.log(req.body); 
+	var newUser = new User({
+		username:req.body.username,
+		firstName:req.body.firstName,
+		lastName:req.body.lastName,
+		email:req.body.email
+	});
+	
+	//add admin manually into database later
+	
+	User.register(newUser, req.body.password, function(err, user) {
+		if(err)
+			{
+				console.log(err); 
+				return res.render("register"); 
+			}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("/games"); 
+		});
+	}); 
+}); 
+
 
 module.exports = router; 
