62970869b796bd2997dba0b1ce67b35e83858f06
Michael Hanson <michaeledwardhanson@gmail.com>
2020-01-30 15:05:54 +0000

    created post route inside multiplication game to submit score (doesn’t post score yet), needs to be styled correctly.

--- routes/games.js
+++ routes/games.js
@@ -51,4 +51,17 @@ router.get("/divisionremainders", function(req, res) {
 	res.render("games/divisionremainders");  
 }); 
 
+//get score form
+router.get("/submit", function(req, res) {
+	console.log("this is the post show route");  //later, show form that will have post route, and then in score.ejs(or something similar, allow user to submit score, passing in unique id to make url unique. later make sure it is authenticated, and signed in)
+	
+	
+	res.render("games/submit"); //only works when logged in currently 
+	
+}); 
+
+router.post("/submit", function(req, res) {
+	res.send("this is the post route"); 
+}); 
+
 module.exports = router; 
