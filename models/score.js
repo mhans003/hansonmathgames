a1bdd70396f4d5f3c96a74fc86ea6bb12aa317c3
Michael Hanson <michaeledwardhanson@gmail.com>
2020-02-05 14:49:59 +0000

    fixed score database problem and added game property to Score model

--- models/score.js
+++ models/score.js
@@ -1,8 +1,35 @@
 var mongoose = require("mongoose"); 
-var passportLocalMongoose = require("passport-local-mongoose"); 
+//var passportLocalMongoose = require("passport-local-mongoose"); 
 
 var ScoreSchema = new mongoose.Schema({
-	value: Number,
+	score: {
+		type: Number, 
+		unique: false,
+		sparse: true
+	},
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			sparse: true
+		},
+		username: {
+			type: String,
+			sparse: true 
+		}
+	},
+	game: {
+		type: String,
+		unique: false,
+		sparse: true
+	}
+});
+
+//ScoreSchema.plugin(passportLocalMongoose); //adds methods into score 
+
+module.exports = mongoose.model("Score", ScoreSchema); 
+
+/*,
 	createdAt: {type: Date, default: Date.now},
 	player: {
 		id: {
@@ -11,9 +38,4 @@ var ScoreSchema = new mongoose.Schema({
 		},
 		username: String
 	},
-	game: String
-});
-
-ScoreSchema.plugin(passportLocalMongoose); //adds methods into score 
-
-module.exports = mongoose.model("Score", ScoreSchema); 
+	game: String*/
--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ var UserSchema = new mongoose.Schema({
 	username: {
 		type: String,
 		unique: true,
-		required: true
+		required: true,
+		sparse: true
 	},
 	password: String,
 	isAdmin: {
