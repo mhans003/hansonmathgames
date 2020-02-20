0a1e680619be300a1994d7908cb2dc76f165e399
Michael Hanson <michaeledwardhanson@gmail.com>
2020-02-20 16:39:04 +0000

    continued refactor with rounding game

--- routes/games.js
+++ routes/games.js
@@ -87,6 +87,21 @@ router.get("/rounding", middleware.isLoggedIn, function(req, res) {
 	}); 
 }); 
 
+router.get("/roundingrefactored", middleware.isLoggedIn, function(req, res) {
+	Score.find({game:"Hanson Rounding Game"},{},{"sort":"score"}, function(err, allscores) {
+		if(err)
+			{
+				console.log(err); 
+				res.redirect("back"); 
+			}
+		else
+			{
+				console.log(allscores);
+				res.render("games/refactors/roundingrefactored", {scores:allscores}); 
+			}
+	}); 
+}); 
+
 router.get("/convertingunits", middleware.isLoggedIn, function(req, res) {
 	Score.find({game:"Hanson Converting Units Game"},{},{"sort":"score"}, function(err, allscores) {
 		if(err)
@@ -197,24 +212,15 @@ router.get("/divisionremaindersrefactored", middleware.isLoggedIn, function(req,
 	}); 
 }); 
 
-//get score form
 router.get("/submit", function(req, res) {
 	console.log("this is the post show route");  //later, show form that will have post route, and then in score.ejs(or something similar, allow user to submit score, passing in unique id to make url unique. later make sure it is authenticated, and signed in)
-	
 	
 	res.render("games/submit"); //only works when logged in currently 
 	
 }); 
 
 router.post("/submit", function(req, res) {
-	//var score = req.body.score; 
-	/*
-	var author = {
-		id: req.user._id,
-		username: req.user.username
-	}; 
-	*/
-	//var newScore = {score:score, author:author}; 
+
 	var newScore = new Score({
 		score:req.body.score,
 		author:{
@@ -223,12 +229,7 @@ router.post("/submit", function(req, res) {
 		},
 		game:req.body.game
 	});
-	//newScore.save(); 
-	//console.log(newScore.value); 
-	//res.send("this is the post route"); 
-	//console.log("value: " + score); 
-	//console.log("newScore: " + newScore); 
-	//console.log("author: " + author.id + "," + author.username); 
+	
 	Score.create(newScore, function(err, createdScore) {
 		if(err)
 			{
@@ -239,14 +240,6 @@ router.post("/submit", function(req, res) {
 		req.flash("success", "New Score Logged"); 
 		res.redirect("back"); 
 	});
-	/*
-	var newScore = new Score({
-		value:req.body.value
-	});
-	newScore.save(); 
-	console.log(newScore.value); 
-	res.send("this is the post route"); 
-	*/
 }); 
 
 router.post("/problems", function(req, res) {
